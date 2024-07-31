caae8f51c015bba627c6e50b46b529f3155ff351
Nikolaos Dimopoulos <nikos@niden.net>
2024-07-31 09:22:35 -0500

    attempt to update the nfr list

---
+++ update-nfr.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+print("Getting NFR Reactions")
+
+result = {}
+
+url = f"{comments}{i}"
+headers = {
+    'Accept': 'application/vnd.github.squirrel-girl-preview+json',
+    'User-Agent': 'Phalcon Agent'
+}
+
+response = requests.get(url, headers=headers)
+content = response.text
+
+print(f"Got content {i}")
+data = json.loads(content)
+
+for comment in data:
+    id = comment.get('id', '')
+    url = comment.get('html_url', '')
+    body = comment.get('body', '')
+    reactions = comment.get('reactions', {})
+    plusone = reactions.get('+1', '')
+
+    body = body.split('\n')[0].replace('\r', '').replace('\r\n', '')
+    plusone = f"{int(plusone):03}"
+    result[f"{plusone}-{id}"] = {
+        'reaction': plusone,
+        'body': f"[{body}]({url})"
+    }
+
+print("Sorting Results")
+sorted_result = dict(sorted(result.items(), key=lambda item: item[0], reverse=True))
+
+print("Creating Content")
+output = "| Votes  | Description             |\n"
+output += "|--------|-------------------------|\n"
+for item in sorted_result.values():
+    output += f"| {item['reaction']} | {item['body']} |\n"
+
+output += "\n"
+
+file_name = './docs/new-feature-request-list.md'
+with open(file_name, 'a') as file:
+    file.write(output)
